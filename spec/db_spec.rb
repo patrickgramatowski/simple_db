5622aaace85173902b09dedde4ba062fa0acce83
patrick <patrick.gramatowski@gmail.com>
2021-03-16 21:26:59 +0100

    Added gemfile/lock and rspec

---
+++ spec/db_spec.rb
@@ -0,0 +1,26 @@
+require "database"
+
+describe Db do
+
+    before do
+        @db = Db.new
+        @db.set("SET A 10")
+    end
+
+    describe ".get" do
+        context "given input 'GET A 10'" do
+            it "returns value of 10" do
+                expect(@db.get("GET A 10")).to eql(["A", "10"])
+            end
+        end
+    end
+
+    describe ".count" do
+        context "given input 'COUNT 10'" do
+            it "returns value of 1" do
+                expect(@db.count("COUNT 10")).to eql(nil)
+            end
+        end
+    end
+
+end
