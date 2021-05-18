b8d35b2b316f21e1b005ec4c1bf7f1c62cd7a2a2
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-05-18 21:49:48 +0200

    refactoring specs

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -2,24 +2,26 @@
 
 require "database"
 
-describe Db do
-  before do
-    @db = Db.new
-    @db.set("SET A 10")
-  end
+RSpec.describe Db do
+  describe Db do
+    before do
+      @db = Db.new
+      @db.set("SET A 10")
+    end
 
-  describe ".get" do
-    context "given input 'GET A 10'" do
-      it "returns value of 10" do
-        expect(@db.get("GET A 10")).to eql(%w[A 10])
+    describe ".get" do
+      context "given input 'GET A 10'" do
+        it "returns value of 10" do
+          expect(@db.get("GET A 10")).to eql(%w[A 10])
+        end
       end
     end
-  end
 
-  describe ".count" do
-    context "given input 'COUNT 10'" do
-      it "returns value of 1" do
-        expect(@db.count("COUNT 10")).to eql(nil)
+    describe ".count" do
+      context "given input 'COUNT 10'" do
+        it "returns value of 1" do
+          expect(@db.count("COUNT 10")).to eql(nil)
+        end
       end
     end
   end
